9908d8ed68c7a4e5992f68c0b381f6a31a563052
Volodymyr <vshvets295@gmail.com>
2022-03-22 22:34:33 +0200

    Added password hashing functions to Util class.

--- src/main/java/model/util/Util.java
+++ src/main/java/model/util/Util.java
@@ -1,10 +1,11 @@
 package model.util;
 
-import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 
 public final class Util {
@@ -47,33 +48,83 @@ public final class Util {
         return sum % 10 == 0;
     }
 
-    public static String generateHashedPassword(String password) {
-        String salt = "1234";
-        int iterations = 1000;
-        int keyLength = 512;
-        char[] passwordChars = password.toCharArray();
-        byte[] saltBytes = salt.getBytes();
-
-        byte[] hashedBytes = hashPassword(passwordChars, saltBytes, iterations, keyLength);
-        return hashedBytes.toString();
-        //return Hex.encodeHexString(hashedBytes);
-    }
-
-    private static byte[] hashPassword(final char[] password, final byte[] salt, final int iterations, final int keyLength) {
-        try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-            PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, keyLength);
-            SecretKey key = skf.generateSecret(spec);
-            byte[] res = key.getEncoded();
-            return res;
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     // ---------------------  hardcoded bad practice, but works if input is CYRILLIC ------------------------
     public static String encode(String text) {
         byte[] bytes = text.getBytes(StandardCharsets.ISO_8859_1);
         return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    public static String hash(String password) {
+        String strongPasswordHash = null;
+        try {
+            strongPasswordHash = generateStrongPasswordHash(password);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+        return strongPasswordHash;
+    }
+
+    private static String generateStrongPasswordHash(String password) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt();
+
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(byte[] array) {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+
+        int paddingLength = (array.length * 2) - hex.length();
+
+        if (paddingLength > 0)
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        else
+            return hex;
+    }
+
+    public static boolean validatePassword(String originalPassword, String storedPassword) {
+        String[] parts = storedPassword.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+
+        byte[] salt = fromHex(parts[1]);
+        byte[] hash = fromHex(parts[2]);
+        int diff = 0;
+
+        try {
+            PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, iterations, hash.length * 8);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] testHash = factory.generateSecret(spec).getEncoded();
+
+            diff = hash.length ^ testHash.length;
+
+            for (int i = 0; i < hash.length && i < testHash.length; i++) {
+                diff |= hash[i] ^ testHash[i];
+            }
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+
+        return diff == 0;
+    }
+
+    private static byte[] fromHex(String hex) {
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
     }
 }
