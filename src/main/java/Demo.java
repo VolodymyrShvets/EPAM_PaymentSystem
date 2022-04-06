061b5f2799527276e980d7d7cfcb98d4b57139a4
Volodymyr <vshvets295@gmail.com>
2022-04-06 21:34:51 +0300

    Cleared Demo.java class.

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,16 +1,4 @@
-import java.time.LocalDate;
-
 public class Demo {
     public static void main(String[] args) {
-        LocalDate now = LocalDate.now();
-        LocalDate date = LocalDate.parse("2022-04-04");
-
-        int comp = date.compareTo(now);
-        if (comp <= 0)
-            System.out.println(date + " is earlier/equal than " + now);
-        //else if (comp == 0)
-        //    System.out.println(date + " is /equal to " + now);
-        else
-            System.out.println(date + " is latter than " + now);
     }
 }
