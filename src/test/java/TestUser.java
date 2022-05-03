c47b566d993dfd9ce5645ac1a9bf985bb0672a82
Volodymyr <vshvets295@gmail.com>
2022-05-03 16:41:03 +0300

    Added classes/methods for testing.

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+public class TestUser {
+    @Test
+    public void testRegisterNewUser1() {
+
+    }
+
+    @Test
+    public void testRegisterNewUser2() {
+
+    }
+
+    @Test
+    public void testValidateUserPassword1() {
+
+    }
+
+    @Test
+    public void testValidateUserPassword2() {
+
+    }
+
+    @Test
+    public void testGetAllUsers() {
+
+    }
+
+    @Test
+    public void testBlockUser() {
+
+    }
+
+    @Test
+    public void testUnblockUser() {
+
+    }
+}
